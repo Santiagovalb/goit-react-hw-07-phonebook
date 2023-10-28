dcce6fefc01ef8633edc430b565b6c91e5da702f
Santiagovalb <santiagovalenciab@gmail.com>
2023-10-28 16:36:18 -0500

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,34 @@
-export const App = () => {
+import React, { useEffect } from 'react';
+
+import Title from './Title/Title';
+import ContactForm from './ContactForm/ContactForm';
+import ContactList from './ContactList/ContactList';
+import Filter from './Filter/Filter';
+import { useDispatch, useSelector } from 'react-redux';
+import { getContacts } from 'redux/operations';
+import { selectContacts } from 'redux/selectors';
+
+export function App() {
+  const { items: contacts, isLoading, error } = useSelector(selectContacts);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getContacts());
+  }, [dispatch]);
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className="form">
+      <Title text="Phonebook" />
+      <ContactForm />
+      {!isLoading && contacts[0] && (
+        <>
+          <Title text="Contacts" />
+          <Filter />
+          <ContactList />
+        </>
+      )}
+      {isLoading && <div style={{ margin: 20 }}>Loading data...</div>}
+      {error && <div style={{ margin: 20 }}>{error}</div>}
     </div>
   );
-};
+}
